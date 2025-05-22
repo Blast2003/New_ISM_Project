f25b378283a685b73b225a8e80919eaddcd5d2b9
Phát <125816527+AkaiShuichi711@users.noreply.github.com>
2025-05-22 17:05:59 +0700

    vercel changed

--- server/index.js
+++ server/index.js
@@ -24,11 +24,11 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html")); // hoặc build/
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist/index.html")); // hoặc build/
+// });
 
 // default middleware
 app.use(express.json());
